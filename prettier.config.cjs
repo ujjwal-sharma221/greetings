f58f74f00dfe0abd6a24eeb1205f2e06e161a37e
Ujjwal Sharma <ujjwalshrm2@gmail.com>
2025-05-31 18:24:14 +0530

    intial-commit

---
+++ prettier.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: ["prettier-plugin-tailwindcss"],
+  semi: true,
+  tabWidth: 2,
+  trailingComma: "es5",
+  printWidth: 100,
+  bracketSpacing: true,
+  arrowParens: "always",
+};
